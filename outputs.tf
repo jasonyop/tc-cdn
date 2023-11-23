7dc96c4645e8e381bf2139590416a9ead37b68ba
openproject <openproject@ipa.sige.la>
2023-11-23 08:09:58 +0000

    [OpenProject] bugfix/5473

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 output "cloud_monitor_access_key" {
   description = "TencentCloud CDN CloudMonitor CAM user's access key id for Grafana CDN dashboard."
   value       = try(tencentcloud_cam_user.cloud_monitor[0].secret_id, "")
+  sensitive   = true
 }
 
 output "cloud_monitor_secret_key" {
@@ -12,6 +13,7 @@ output "cloud_monitor_secret_key" {
 output "game_deploy_access_key" {
   description = "TencentCloud CDN CAM user's access key id for game deploy."
   value       = try(tencentcloud_cam_user.game_deploy_user[0].secret_id, "")
+  sensitive   = true
 }
 
 output "game_deploy_secret_key" {
